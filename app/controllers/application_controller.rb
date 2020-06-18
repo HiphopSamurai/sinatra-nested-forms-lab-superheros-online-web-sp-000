28f9303cc5e147b8e16ed6292c7e315a1ca25eb8
Garrett Thompson <gtcruz.gt@gmail.com>
2020-06-18 00:39:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/super_hero' do
+      @team = Team.new(params[:team])
+      erb :'/team'
+    end
 
 end
